41bc9a507121cc93114126253861441ada2f6fc6
Alexandre Gonçalves <88319848+AlexandreFPGoncalves@users.noreply.github.com>
2022-11-06 04:54:05 +0000

    initial repository setup

---
+++ rollup.config.js
@@ -0,0 +1,58 @@
+import babel from 'rollup-plugin-babel';
+import resolve from '@rollup/plugin-node-resolve';
+import external from 'rollup-plugin-peer-deps-external';
+import postcss from 'rollup-plugin-postcss';
+import typescript from '@rollup/plugin-typescript';
+import commonjs from '@rollup/plugin-commonjs';
+import dts from 'rollup-plugin-dts';
+import tailwind from 'rollup-plugin-tailwindcss';
+import { terser } from 'rollup-plugin-terser';
+
+const packageJson = require('./package.json');
+export default [
+	{
+		input: './src/index.js',
+		//exports as commonJS and ESModules
+		//Each item of the following array will be a different configuration for each type of output
+		output: [
+			//commonJS Configuration
+			{
+				file: packageJson.main,
+				format: 'cjs',
+				sourcemap: true,
+			},
+			//ES6 Configuration
+			{
+				file: packageJson.module,
+				format: 'esm',
+				sourcemap: true,
+			},
+		],
+		plugins: [
+			postcss({
+				plugins: [],
+				minimize: true,
+			}),
+			tailwind({
+				input: './src/styles/global.css',
+				purge: false,
+			}),
+			typescript({ tsconfig: './tsconfig.json' }),
+			babel({
+				exclude: 'node_modules/**',
+				presets: ['@babel/preset-react', '@babel/preset-typescript'],
+			}),
+			external(),
+			resolve({
+				extensions: ['.js', '.jsx', '.jsox', '.ts', '.tsx'],
+			}),
+			commonjs(),
+			terser(),
+		],
+	},
+	{
+		input: 'dist/esm/index.d.ts',
+		output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+		plugins: [dts()],
+	},
+];
