1d765c598a9b509fde906570ccf20ba0d2bf72ea
Alexandre Gonçalves <88319848+AlexandreFPGoncalves@users.noreply.github.com>
2023-04-20 16:34:38 +0100

    📝 Update Application Version

--- MockApplication/src/App.tsx
+++ MockApplication/src/App.tsx
@@ -1,28 +1,29 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.png";
-import { ButtonComponent } from "react-component-library-itsector-workshop";
-import "./App.css";
+import { useState } from 'react';
+import { ButtonComponent } from 'react-component-library-itsector-workshop';
+import reactLogo from './assets/react.png';
+
+import './styles/App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+	const [count, setCount] = useState(0);
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>React Mock Application</h1>
-      <div className="card">
-        <ButtonComponent
-          type="primary"
-          onClick={() => setCount((count) => count + 1)}
-          label={`count is ${count}`}
-        />
-      </div>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div>
+				<a href="https://reactjs.org" target="_blank">
+					<img src={reactLogo} className="logo react" alt="React logo" />
+				</a>
+			</div>
+			<h1>React Mock Application</h1>
+			<div className="card">
+				<ButtonComponent
+					type="primary"
+					onClick={() => setCount((count) => count + 1)}
+					label={`count is ${count}`}
+				/>
+			</div>
+		</div>
+	);
 }
 
 export default App;
